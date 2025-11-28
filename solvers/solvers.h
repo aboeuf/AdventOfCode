c464adefbea8089b8cc0097336002ea5710347ec
Alexandre Boeuf <alexandre.boeuf@easymile.com>
2025-11-28 15:54:33 +0100

    Clean up file architecture

---
+++ solvers/solvers.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QMap>
+#include <QObject>
+#include <QString>
+#include <gui/display/display.h>
+
+class Solver : public QObject {
+  Q_OBJECT
+
+signals:
+  void finished(const QString &output);
+  void output(const QString &data);
+
+public:
+  virtual ~Solver() {}
+  virtual void solve(const QString &input) = 0;
+  Display *m_display{nullptr};
+};
+
+struct Solvers {
+  Solvers();
+  ~Solvers();
+
+  Solver *operator()(int year, int day, int puzzle) const;
+  QMap<int, QMap<int, QMap<int, Solver *>>> m_solvers;
+};
